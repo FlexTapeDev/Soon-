0fd919c6f1a484e948d4edf3dc16f7bb8194267b
FlexTape <145163781+FlexTapeDev@users.noreply.github.com>
2023-11-15 23:06:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ let lastScrollY = 0;
 
 document.addEventListener('scroll', () => {
     const element = document.getElementById('animatedElement');
+    const lines = document.querySelectorAll('.line');
     const scrollY = window.scrollY;
 
-    // Slow rotation based on scroll position
-    const rotateY = scrollY * 0.1; // Horizontal rotation
-    const rotateX = scrollY * 0.1; // Vertical rotation
+    // Rotation based on scroll position
+    const rotateY = scrollY * 0.1;
+    const rotateX = scrollY * 0.1;
+
+    // Invert colors based on scroll position
+    const backgroundColorValue = Math.min(255, scrollY / 4);
+    const lineColorValue = 255 - backgroundColorValue;
+
+    // Applying styles
+    document.body.style.backgroundColor = `rgb(${backgroundColorValue}, ${backgroundColorValue}, ${backgroundColorValue})`;
+    lines.forEach(line => {
+        line.style.borderColor = `rgb(${lineColorValue}, ${lineColorValue}, ${lineColorValue})`;
+    });
 
     element.style.transform = `translate(-50%, -50%) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 
