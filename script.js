9c6d3f215b23c7610dfef876cf4ac52aa9c9a7c2
FlexTape <145163781+FlexTapeDev@users.noreply.github.com>
2023-11-15 22:18:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let lastScrollY = 0;
 
+document.addEventListener('scroll', () => {
+    const element = document.getElementById('animatedElement');
+    const scrollY = window.scrollY;
+
+    // Slow rotation based on scroll position
+    const rotateY = scrollY * 0.1; // Horizontal rotation
+    const rotateX = scrollY * 0.1; // Vertical rotation
+
+    element.style.transform = `translate(-50%, -50%) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+    lastScrollY = scrollY;
+});
